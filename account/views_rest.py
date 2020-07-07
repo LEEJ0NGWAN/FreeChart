dfcc6bfaa2cebeb56a62b1f4df41e856ff68cb91
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-07 15:58:03 +0900

    로그인 로그아웃 뼈대

--- account/views_rest.py
+++ account/views_rest.py
@@ -1,1 +1,16 @@
 # TODO: login & logout
+
+
+@api_view(["POST"])
+@csrf_exempt
+def login(request):
+    """
+    {string} username(email)
+    {string} password
+    """
+    pass
+
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def logout(request):
+    pass
