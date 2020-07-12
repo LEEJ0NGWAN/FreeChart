ab4e4b68bdad8544fb5094d060805b61af72e878
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-13 01:14:28 +0900

    board api

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
 from account.api.user import UserController
 
-# TODO: user/api
+# TODO: board
 urlpatterns = [
-    path('user/', UserController.as_view(),
-         name='UserView'),
-#     path('user/phone/verify/', PhoneVerifyView.as_view(),
-#          name='PhoneVerifyView'),
-#     path('user/password/', UserPasswordView.as_view(),
-#          name='UserPassword'),
-#     path('user/findpassword/', UserFindPasswordView.as_view(),
-#          name='UserFindPassword'),
-#     path('user/search/', UserSearchView.as_view(),
-#          name='UserSearch'),
+    path(
+        'user/', 
+        UserController.as_view(), 
+        name='UserView'),
+    # path(
+    #     'board/', 
+    #     BoardController.as_view(),
+    #     name='BoardView'),
 ]
 
