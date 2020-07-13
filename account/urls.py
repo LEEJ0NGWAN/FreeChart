9b0a0e56bbd99e56b10d11bb171f0e3d618f312c
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-13 21:58:48 +0900

    사용자 비밀번호 재설정 구현

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('login/', views_rest.login),
     path('logout/', views_rest.Logout.as_view()),
     path('email/verify/', views_rest.EmailVerify.as_view()),
-    # TODO: 비밀번호 재설정 api
-    # path('password/reset/', views_rest.PasswordReset.as_view()),
+    path('password/reset/', views_rest.PasswordReset.as_view()),
 ]
 
--- account/views_rest.py
+++ account/views_rest.py
@@ -140,14 +140,74 @@ class EmailVerify(View):
 
         return JsonResponse({})
 
-# TODO: SMTP + REDIS
+@method_decorator(csrf_exempt, name='dispatch')
 class PasswordReset(View):
-    def get(self, request):
-        return
     def post(self, request):
-        return
-    def put(self, request):
-        return
-    def delete(self, request):
-        return
+        if request.POST:
+            data = request.POST
+        else:
+            data = json.loads(request.body.decode("utf-8"))
+
+        if 'email' not in data:
+            return JsonResponse({
+                'error': 'no email'
+            }, status=HTTP_400_BAD_REQUEST)
+
+        email = data.get('email')
+
+        if not User.objects.filter(email=email).exists():
+            return JsonResponse({
+                'error': 'no user'
+            }, status=HTTP_404_NOT_FOUND)
+
+        key = f'RESET:{email}'
+        tmp_key = f'TMP:{email}'
+
+        if 'token' in data:
+            token = redis.get(key)
+            tmp_password = redis.get(tmp_key)
+
+            if not token:
+                return JsonResponse({
+                    'error': 'no token'
+                }, status=HTTP_404_NOT_FOUND)
+            
+            if token != data.get('token'):
+                return JsonResponse({
+                    'error': 'incorrect'
+                }, status=HTTP_400_BAD_REQUEST)
+
+            user = User.objects.filter(email=email).first()
+            user.set_password(tmp_password)
+            user.save()
+
+            del redis[key]
+            del redis[tmp_key]
+
+        else:
+            token = id_generator(size=128)
+            tmp_password = id_generator(size=12)
+
+            redis.set(key, token, 180) # 3분
+            redis.set(tmp_key, tmp_password, 180)
+
+            payload = '{"email": %s, "token": %s}'%(email, token)
+            html = loader.render_to_string(
+                'tmp_password_template.html',
+                {
+                    'password': tmp_password,
+                    'email': email,
+                    'token': token
+                }
+            )
+
+            send_mail(
+                '[FreeList] 비밀번호 재설정 링크',
+                '',
+                'no-reply@freelist.tk',
+                [email],
+                html_message=html
+            )
+
+        return JsonResponse({})
 
