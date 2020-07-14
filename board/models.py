5627745410a2cc0df836d2cf15c3365b964d490d
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-14 16:22:08 +0900

    재사용을 위한 유저 모델 설정

--- board/models.py
+++ board/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from FreeList.settings import AUTH_USER_MODEL
 
 class Board(models.Model):
     class Meta:
         db_table = 'board'
         verbose_name = 'board'
-    owner = models.ForeignKey('account.User', on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=256, verbose_name='제목')
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='작성 날짜')
     modify_date = models.DateTimeField(auto_now=True, verbose_name='수정 날짜')
@@ -13,7 +14,7 @@ class Sheet(models.Model):
     class Meta:
         db_table = 'sheet'
         verbose_name = 'sheet'
-    owner = models.ForeignKey('account.User', on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     board = models.ForeignKey('board.Board', on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=256, verbose_name='이름')
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='작성 날짜')
