0031a27fafa880e7ce869606be3a008f10d1197c
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-14 15:32:17 +0900

    sheet 모델 생성

--- board/models.py
+++ board/models.py
@@ -9,3 +9,13 @@ class Board(models.Model):
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='작성 날짜')
     modify_date = models.DateTimeField(auto_now=True, verbose_name='수정 날짜')
 
+class Sheet(models.Model):
+    class Meta:
+        db_table = 'sheet'
+        verbose_name = 'sheet'
+    owner = models.ForeignKey('account.User', on_delete=models.SET_NULL, null=True)
+    board = models.ForeignKey('board.Board', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=256, verbose_name='이름')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='작성 날짜')
+    modify_date = models.DateTimeField(auto_now=True, verbose_name='수정 날짜')
+
