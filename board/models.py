3204a3c77d2816f3114699a785ac931d5e71ee65
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-14 14:38:43 +0900

    board 모델 생성 및 앱 등록

--- board/models.py
+++ board/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+class Board(models.Model):
+    class Meta:
+        verbose_name = 'board'
+    owner = models.ForeignKey('account.User', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=256, verbose_name='제목')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='작성 날짜')
+    modify_date = models.DateTimeField(auto_now=True, verbose_name='수정 날짜')
+
