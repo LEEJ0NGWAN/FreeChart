5fb26c6a3c58adc87977d77dc79e043e3654e295
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-14 16:18:54 +0900

    node & edge 모델 생성

--- board/models.py
+++ board/models.py
@@ -19,3 +19,27 @@ class Sheet(models.Model):
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='작성 날짜')
     modify_date = models.DateTimeField(auto_now=True, verbose_name='수정 날짜')
 
+class Node(models.Model):
+    class Meta:
+        db_table = 'node'
+        verbose_name = 'node'
+    sheet = models.ForeignKey('board.Sheet', on_delete=models.SET_NULL, null=True)
+    label = models.CharField(max_length=128, verbose_name='라벨')
+
+class Edge(models.Model):
+    class Meta:
+        db_table = 'edge'
+        verbose_name = 'edge'
+    sheet = models.ForeignKey('board.Sheet', on_delete=models.SET_NULL, null=True)
+    label = models.CharField(max_length=128, verbose_name='라벨')
+    node1 = models.ForeignKey(
+        'board.Node',
+        on_delete=models.CASCADE,
+        related_name='node1'
+    )
+    node2 = models.ForeignKey(
+        'board.Node',
+        on_delete=models.CASCADE,
+        related_name='node2'
+    )
+
