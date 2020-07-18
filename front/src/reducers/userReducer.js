c88fb8a97417bc0295e47b141989687e7bd57816
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-18 15:58:58 +0900

    reducers 초기 생성

---
+++ front/src/reducers/userReducer.js
@@ -0,0 +1,15 @@
+import { GET_USER } from '../actions/api';
+
+const initialState = {
+    user: null,
+};
+
+export default function (state = initialState, action) {
+    switch(action.type) {
+        case GET_USER:
+            return { ...state, user: action.payload.data };
+        default:
+            return state;
+    }
+}
+
