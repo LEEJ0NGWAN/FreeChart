3c6ae18d0d3d2a96646b6262bb838a6e679683d2
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-18 15:16:35 +0900

    index: 앱 전체를 장악하는 store, router history 설정

---
+++ front/src/actions/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import configureStore from './store/configureStore';
+import createHistory from 'history/createBrowserHistory';
+import { Router, Route} from 'react-router-dom'
+
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={createHistory()}>
+            <Route path="/" component={App}/>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
+
