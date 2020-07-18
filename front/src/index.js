faee80b9c0efe2d949b69143ba590e8d95e102f9
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-18 17:09:20 +0900

    reducer, store, history 생성 관련 fix

--- front/src/index.js
+++ front/src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import configureStore from './store/configureStore';
+import { createStore } from 'redux'
+import rootReducer from './reducers/rootReducer';
 import createHistory from 'history/createBrowserHistory';
 import { Router, Route} from 'react-router-dom'
 
-const store = configureStore();
+const store = createStore(rootReducer);
+const history = createHistory();
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={createHistory()}>
+        <Router history={history}>
             <Route path="/" component={App}/>
         </Router>
     </Provider>,
--- front/src/reducers/rootReducer.js
+++ front/src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer } from 'redux-form';
 import UserReducer from './userReducer';
 
 const rootReducer = combineReducers({
     user: UserReducer,
-    form: reducer
 });
 export default rootReducer;
 
