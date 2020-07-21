55612aded9614b573906dd4f66d415f3ef555064
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-21 20:59:47 +0900

    이상한 홈

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default function() {
-    return (
-        <div>
-            FreeList
-        </div>
-    );
+class Home extends Component {
+    componentDidMount() {
+        const {logged, history} = this.props;
+        if (logged) return;
+        
+        history.push('/login');
+
+    }
+
+    componentDidUpdate() {
+        // pass
+    }
+  
+    renderUser() {
+        return JSON.stringify(this.props.user);
+    }
+  
+    render() {
+        return (
+            <div>
+            <h2>Home</h2>
+            <ul>
+                {this.renderUser()}
+            </ul>
+            </div>
+        );
+    }
 }
+
+export default connect((state) => {
+    return {
+      user: state.userReducer.user,
+      logged: state.userReducer.logged
+    };
+  }, {})(Home);
 
