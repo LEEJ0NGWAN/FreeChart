6d235c08d6248b2a441719a7b0f6da862b96e1e5
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-21 20:56:47 +0900

    ERROR 액션은 공용으로 사용될 수 있기에 공통 리듀서로 독립시킴, 공용 state를 지우는 CLEAR 액션

---
+++ front/src/reducers/commonReducer.js
@@ -0,0 +1,31 @@
+import { CLEAR, ERROR } from '../actions/api';
+
+const initialState = {
+    error_msg: null,
+    error_code: null
+};
+
+export default function (state = initialState, action) {
+    switch(action.type) {
+        case CLEAR:
+            switch (action.payload) {
+                case ERROR:
+                    return {
+                        ...state,
+                        error_msg: null,
+                        error_code: null,
+                    };
+                default:
+                    return state;
+            }
+        case ERROR:
+            return {
+                ...state,
+                error_msg: action.payload.error_msg,
+                error_code: action.payload.error_code
+            };
+        default:
+            return state;
+    }
+}
+
