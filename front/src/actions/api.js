03eb084d415b90ca54c07aba062131db7f18dd8a
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-21 20:55:33 +0900

    login, logout api의 액션 디스패치 수정, 이메일과 닉네임 중복검사 액션 check 추가

--- front/src/actions/api.js
+++ front/src/actions/api.js
@@ -2,9 +2,11 @@ import { fetchData } from './fetch';
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
+export const CLEAR = 'CLEAR'; // 에러와 같은 공용 state에 대한 clear
 export const ERROR = 'ERROR';
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
+export const CHECK = 'CHECK';
 
 export function login(email, password) {
     return (dispatch) => {
@@ -16,13 +18,14 @@ export function login(email, password) {
             })
             .then(res => {
                 dispatch(fetchData(LOGIN, res.data));
+                dispatch(fetchData(CLEAR, ERROR));
             })
             .catch(err => {
-                let data = {
-                    msg: err.response.data.error,
-                    status: err.response.status
+                let payload = {
+                    error_msg: err.response.data.error,
+                    error_code: err.response.status
                 };
-                dispatch(fetchData(ERROR, data));
+                dispatch(fetchData(ERROR, payload));
             });
     };
 }
@@ -32,10 +35,44 @@ export function logout() {
         return axios.post('api/account/logout/')
         .then(res => {
             dispatch(fetchData(LOGOUT, {}));
+            dispatch(fetchData(CLEAR, ERROR));
         })
         .catch(err => {
-            throw(err);
+            let payload = {
+                error_msg: err.response.data.error,
+                error_code: err.response.status
+            };
+            dispatch(fetchData(ERROR, payload));
         });
     };
+}
+
+export function check(email=null, username=null) {
+    return (dispatch) => {
+        let data = {};
+        if (email){
+            data.email = email;
+        }
+
+        if (username){
+            data.username = username;
+        }
+
+        let length = Object.keys(data).length;
+        if (!length) return;
+        
+        return axios.post('api/account/check/', data)
+        .then(res => {
+            dispatch(fetchData(CHECK, res.data));
+            dispatch(fetchData(CLEAR, ERROR));
+        })
+        .catch(err => {
+            let payload = {
+                error_msg: err.response.data.error,
+                error_code: err.response.status
+            };
+            dispatch(fetchData(ERROR, payload));
+        })
+    }
 }
 
