6757a1b19ebbc1dafd6dee9d2bb9810f863fe1e4
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-26 23:01:14 +0900

    board들과 보드에 속하지 않은 sheet들 렌더링

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -6,14 +6,16 @@ import { BOARDS, SHEETS, getBoard } from '../actions/board_api';
 
 class Home extends Component {
     initializeUserInfo = async () => {
-        const root = JSON.parse(localStorage.getItem('root'));
-        if(!root) {
+        const boards = JSON.parse(localStorage.getItem('boards'));
+        const sheets = JSON.parse(localStorage.getItem('sheets'));
+
+        if(!boards || !sheets) {
             await this.props.getBoard();            
             return;
         }
 
-        this.props.fetch(BOARDS, {boards: root.boards});
-        this.props.fetch(SHEETS, {sheets: root.sheets});
+        this.props.fetch(BOARDS, {boards: boards});
+        this.props.fetch(SHEETS, {sheets: sheets});
     }
 
     componentDidMount() {
@@ -27,25 +29,56 @@ class Home extends Component {
 
     componentDidUpdate(prevProps, prevStates) {
         const {boards, sheets} = this.props;
-        
-        let root = {};
-        if (prevProps.boards != boards)
-            root.boards = boards;
 
-        if (prevProps.sheets != sheets)
-            root.sheets = sheets;
-
-        if (Object.keys(root).length){
+        if (prevProps.boards !== boards){
             localStorage.setItem(
-                'root',
-                JSON.stringify(root));
+                'boards',
+                JSON.stringify(boards));
+        }
+
+        if (prevProps.sheets !== sheets){
+            localStorage.setItem(
+                'sheets',
+                JSON.stringify(sheets));
         }
     }
   
     renderUser() {
-        return JSON.stringify(this.props.user);
+        const {user} = this.props;
+        if (!user)
+            return;
+        
+        let userInfo = [];
+        userInfo.push(<div key={user.email}>{user.email}<br/></div>);
+        userInfo.push(<div key={user.username}>{user.username}<br/></div>);
+
+        return userInfo;
     }
-  
+
+    renderBoards() {
+        const {boards} = this.props;
+        if (!boards)
+            return;
+        
+        let boardList = [];
+        for (let board of boards) {
+            boardList.push(<li key={board.id}><b>{board.title}</b></li>)
+        }
+        return boardList;
+    }
+
+    renderSheets() {
+        const {sheets} = this.props;
+        if (!sheets)
+            return;
+
+        let sheetList = [];
+        for (let sheet of sheets) {
+            sheetList.push(<li key={sheet.id}><b>{sheet.title}</b></li>)
+        }
+        return sheetList;
+    }
+
     render() {
         const logout = (
             <button className="waves-effect waves-light btn">
@@ -56,10 +89,8 @@ class Home extends Component {
         return (
             <div>
             <h1><b>{this.renderUser()}</b></h1><br/>
-            {logout}<br/>
-            <ul>
-                {this.renderUser()}
-            </ul>
+            {this.renderBoards()}<br/>
+            {this.renderSheets()}<br/>
             </div>
         );
     }
