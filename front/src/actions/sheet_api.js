fb5c6b48049183744ac2221be238a90f984e1141
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-27 16:34:38 +0900

    api 호출 수정

--- front/src/actions/sheet_api.js
+++ front/src/actions/sheet_api.js
@@ -7,7 +7,7 @@ export const ELEMENTS = 'ELEMENTS';
 export function getElement(sheet_id) {
     return (dispatch) => {
         let params = {sheet_id: sheet_id};
-        return axios.get('api/sheet/element', {params})
+        return axios.get('api/sheet/element/', {params})
         .then(res => {
             dispatch(fetch(ELEMENTS, res.data));
             dispatch(clearError());
