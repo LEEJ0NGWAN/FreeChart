ab03a441093f0439adbb7dc1864d185bdb9d2d70
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-28 18:31:17 +0900

    sheet에서 노드 더블 클릭 시 노드 수정 모달 띄우기

--- front/src/components/Sheet.js
+++ front/src/components/Sheet.js
@@ -4,6 +4,7 @@ import { clearError } from '../actions/common';
 import { getElement } from '../actions/sheet_api';
 import { fetch } from '../actions/common';
 import Graph from 'react-graph-vis';
+import NodeEdit from './NodeEdit';
 
 const options = {
     layout: {
@@ -11,55 +12,77 @@ const options = {
     },
     edges: {
         color: "#000000"
-    },
+    }
 };
 
 class Sheet extends Component {
     state = {
+        popped: false,
+        isChanged: false,
         error: null,
     };
 
     events = {
+        // hold: function(event) {
+        // },
         // click: function(event) {
-        //     this.redraw();
         // },
         // select: function(event) {
         //     var { nodes, edges } = event;
-        //     console.log("Selected nodes:");
-        //     console.log(nodes);
-        //     console.log("Selected edges:");
-        //     console.log(edges);
+        //     if (!this.selected)
+        //         this.selected = nodes
+        //     console.log(this.selected, nodes);
         // },
         doubleClick: function(event) {
-            let node = {
-                id: this.state.index,
-                label: 'test',
-                sheet_id: Number(this.props.sheet_id)
-            };
-            this.setState({
-                graph: {
-                    ...this.state.graph,
-                    nodes: [
-                        ...this.state.graph.nodes,
-                        node
-                    ]
-                },
-                index: (this.state.index+1)
-            });
-            console.log(this);
+            const {nodes} = event;
+            if (!nodes.length) {
+                const {x, y} = event.pointer.canvas;
+                let node = {
+                    id: this.state.index,
+                    label: 'test',
+                    sheet_id: Number(this.props.sheet_id),
+                    x: x, y: y,
+                    title: 'test'
+                };
+                this.setState({
+                    graph: {
+                        ...this.state.graph,
+                        nodes: [
+                            ...this.state.graph.nodes,
+                            node
+                        ]
+                    },
+                    index: (this.state.index+1)
+                });
+            }
+            else {
+                const {x, y} = event.pointer.DOM;
+                this.togglePop(x,y);
+            }
         }.bind(this),
 
         // dragging: function(event) {
-
         // }
-    }; 
+    };
+
+    togglePop = (x=null, y=null) => {
+        let nextState = {
+            popped: !this.state.popped
+        };
+        if (x)
+            nextState.x = x;
+        if (y)
+            nextState.y = y;
+
+        this.setState(nextState);
+    }
 
     fetchElements = async () => {
         const {sheet_id} = this.props;
         await this.props.getElement(sheet_id);
 
         const {nodes} = this.props;
-        let index = nodes[nodes.length-1].id + 1;
+        let index = nodes.length? nodes[nodes.length-1].id + 1: 0;
         this.setState({
             graph: {
                 nodes: this.props.nodes,
@@ -88,14 +111,24 @@ class Sheet extends Component {
     render() {
         return (
             <div>
-                {this.state.error}
-                {this.state.graph &&
-                <Graph 
-                graph={this.state.graph} 
-                options={options} 
-                events={this.events} 
-                style={{ height: "640px" }} />}
+                <div>
+                    {this.state.error}
+                    {this.state.popped && 
+                    <NodeEdit 
+                    togglePop={this.togglePop} 
+                    x={this.state.x}
+                    y={this.state.y}/>}
+                </div>
+                <div>
+                    {this.state.graph &&
+                    <Graph 
+                    graph={this.state.graph} 
+                    options={options} 
+                    events={this.events}
+                    style={{position: 'absolute', width:'100%', height: '65%'}}/>}
+                </div>
             </div>
+
         )
     }
 }
