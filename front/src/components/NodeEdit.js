4f82c69eddf1830f066c6b63e4a9b077263c6721
LEEJ0NGWAN <lastffx@gmail.com>
2020-07-30 22:30:20 +0900

    기존 내용 디폴트 값으로 부르기 및 엔터와 esc키 사용과 자동 포커스

--- front/src/components/NodeEdit.js
+++ front/src/components/NodeEdit.js
@@ -7,38 +7,73 @@ class NodeEdit extends Component {
         ref: React.createRef()
     }
 
+    componentDidMount() {
+        this.labelInput.focus();
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps) {
+        this.setState({
+            label: nextProps.label
+        });
+    }
+
     changer = (event) => {
         let nextState = {};
         nextState[event.target.name] = event.target.value;
         this.setState(nextState);
     }
 
+    processor = (event) => {
+        switch(event.target.name) {
+            case 'label':
+            case 'modify':
+                if(this.state.label !== this.props.label)
+                    this.props.modifyNode(this.state.label);
+                break;
+            case 'delete':
+                this.props.deleteNode();
+                break;
+            default:
+                break;
+        }
+        this.props.togglePop();
+    }
+
     render() {
         let x = this.props.x+'px';
         let y = this.props.y+'px';
         return(
-            <div className="modal"
+            <div 
+            className="modal" 
             onClick={()=>{this.props.togglePop();}}>
                 <div
+                className="modal_content"
                 onClick={(e)=>{e.stopPropagation();}}
                 ref={this.state.ref}
-                className="modal_content"
                 style={{
                     top: y,
                     left: x
                 }}>
-                    <input 
+                    <input
                     name="label" 
-                    type="text" 
+                    type="text"
                     value={this.state.label}
-                    onChange={this.changer}/>
-                    <button
-                    onClick={()=>{
-                        this.props.fetchInfo(
-                            this.props.nodeId,
-                            null,null,
-                            this.state.label,true)}}>수정</button>
-                    <button>삭제</button>
+                    onChange={this.changer}
+                    onKeyPress={(e)=>{
+                        if (e.key === "Enter")
+                            this.processor(e);
+                    }}
+                    onKeyDown={(e)=>{
+                        if (e.key === "Escape")
+                            this.props.togglePop();
+                    }}
+                    ref={(input)=>{this.labelInput = input}}/>
+                    <button 
+                    name='modify' 
+                    onClick={this.processor}>수정</button>
+                    <button 
+                    name='delete' 
+                    onClick={this.processor}>삭제</button>
                 </div>
                 
             </div>
