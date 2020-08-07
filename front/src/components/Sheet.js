e7160604ebd9871249d42ccc272768537d2d6fd1
LEEJ0NGWAN <lastffx@gmail.com>
2020-08-07 19:48:25 +0900

    시트 엘리먼트 아이콘으로 대치 및 구조 재구성

--- front/src/components/Sheet.js
+++ front/src/components/Sheet.js
@@ -7,6 +7,16 @@ import Graph from 'react-graph-vis';
 import NodeEdit from './NodeEdit';
 import { v4 as uuid } from 'uuid';
 
+const style = {
+    position: 'absolute',
+    left: '5px',
+    right: '5px',
+    bottom: '5px',
+    height: 'calc(100% - 100px)',
+    border: '1px solid #000000',
+    borderRadius: '1em'
+};
+
 const options = {
     layout: {
         hierarchical: false
@@ -252,55 +262,69 @@ class Sheet extends Component {
         }
     }
 
+    renderBackIcon() {
+        return(<svg className="bs-item"
+        onClick={()=>{this.props.escape();}}
+        width="24" height="24" viewBox="0 0 24 24">
+        <path d="M13.427 3.021h-7.427v-3.021l-6 5.39 6 
+        5.61v-3h7.427c3.071 0 5.561 2.356 5.561 5.427 0 
+        3.071-2.489 5.573-5.561 5.573h-7.427v5h7.427c5.84 0 
+        10.573-4.734 10.573-10.573s-4.733-10.406-10.573-10.406z"/>
+        </svg>);
+    }
+
+    renderSaveIcon() {
+        return(<svg className="bs-item"
+        onClick={this.save}
+        width="24" height="24" viewBox="0 0 24 24">
+        <path d="M13 3h2.996v5h-2.996v-5zm11 
+        1v20h-24v-24h20l4 4zm-17 
+        5h10v-7h-10v7zm15-4.171l-2.828-2.829h-.172v9h-14v-9h-3v20h20v-17.171z"/>
+        </svg>);
+    }
+
+    renderRefreshIcon() {
+        return(<svg className="bs-item"
+        onClick={this.cancel}
+        width="24" height="24" viewBox="0 0 24 24">
+        <path d="M20.944 12.979c-.489 4.509-4.306 
+        8.021-8.944 8.021-2.698 0-5.112-1.194-6.763
+        -3.075l1.245-1.633c1.283 1.645 3.276 2.708 
+        5.518 2.708 3.526 0 6.444-2.624 6.923-6.021h
+        -2.923l4-5.25 4 5.25h-3.056zm-15.864-1.979c.487-3.387 
+        3.4-6 6.92-6 2.237 0 4.228 1.059 5.51 2.698l1.244
+        -1.632c-1.65-1.876-4.061-3.066-6.754-3.066-4.632 
+        0-8.443 3.501-8.941 8h-3.059l4 5.25 4-5.25h-2.92z"/></svg>);
+    }
+
     render() {
-        const save = (
-            <p className="control item" style={{marginLeft:'10px'}}
-            onClick={this.save}>변경 사항 저장</p>
-        )
-        const cancel = (
-            <p className="control item" style={{marginLeft:'10px'}}
-            onClick={this.cancel}>취소</p>
-        )
-        const control = (
-            <div>
-                <p className="control item"
-                onClick={()=>{this.props.escape();}}>나가기</p>
-                {this.isEdited() && save}
-                {this.isEdited() && cancel}<br/>
+        const menu = (
+            <div className="sheet-menu">
+                {this.renderBackIcon()}
+                {this.isEdited() && this.renderSaveIcon()}
+                {this.isEdited() && this.renderRefreshIcon()}<br/>
             </div>
         )
         return (
             <div>
-                {control}
-                <div>
-                    {this.state.popped && 
-                    <NodeEdit 
-                    togglePop={this.togglePop}
-                    modifyNode={this.modifyNode}
-                    deleteNode={this.deleteNode}
-                    nodeId={this.state.nodeId}
-                    x={this.state.x}
-                    y={this.state.y}
-                    label={this.state.label}/>}
-                </div>
-                <div>
-                    {this.state.graph &&
-                    <Graph
-                    ref={this.state.networkRef}
-                    graph={this.state.graph} 
-                    options={options} 
-                    events={this.events}
-                    style={{
-                        position: 'absolute',
-                        left: '5px',
-                        right: '5px',
-                        bottom: '5px',
-                        height: 'calc(100% - 70px)',
-                        border: '1px solid #000000',
-                        borderRadius: '1em'}}/>}
-                </div>
+                {menu}
+                {this.state.popped && 
+                <NodeEdit 
+                togglePop={this.togglePop}
+                modifyNode={this.modifyNode}
+                deleteNode={this.deleteNode}
+                nodeId={this.state.nodeId}
+                x={this.state.x}
+                y={this.state.y}
+                label={this.state.label}/>}
+                {this.state.graph &&
+                <Graph
+                ref={this.state.networkRef}
+                graph={this.state.graph} 
+                options={options} 
+                events={this.events}
+                style={style}/>}
             </div>
-
         )
     }
 }
