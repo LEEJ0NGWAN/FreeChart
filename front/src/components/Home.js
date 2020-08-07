05ac4136dc499895332d99072fb2cbdd277f194e
LEEJ0NGWAN <lastffx@gmail.com>
2020-08-07 18:27:45 +0900

    홈버튼

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -158,6 +158,15 @@ class Home extends Component {
         </svg>);
     }
 
+    renderHomeIcon() {
+        return(<svg className="bs-item"
+        onClick={this.initialize}
+        width="24" height="24" viewBox="0 0 24 24">
+        <path d="M20 7.093v-5.093h-3v2.093l3 3zm4 
+        5.907l-12-12-12 12h3v10h7v-5h4v5h7v-10h3zm-5 
+        8h-3v-5h-8v5h-3v-10.26l7-6.912 7 6.99v10.182z"/></svg>);
+    }
+
     renderBoards() {
         const {boards} = this.props;
         if (!boards)
@@ -214,17 +223,12 @@ class Home extends Component {
     }
 
     render() {
-        // TODO: 나중에 홈버튼으로 쓰기
-        // const back = (
-        //     <label className="bs-item" 
-        //     style={{display:'block'}}
-        //     onClick={this.initialize}>←</label>
-        // )
         const menu = (
             <div className="home-menu">
             {this.state.boardId && this.renderUndoIcon()}
             {this.renderNewFolderIcon()}
             {this.renderNewFileIcon()}
+            {this.state.boardId && this.renderHomeIcon()}
             </div> 
         )
         return (
