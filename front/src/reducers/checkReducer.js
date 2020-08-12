5a0db3038cd94fa3af7175b84b369d274b0d637b
LEEJ0NGWAN <lastffx@gmail.com>
2020-08-13 01:01:37 +0900

    fix

--- front/src/reducers/checkReducer.js
+++ front/src/reducers/checkReducer.js
@@ -1,4 +1,6 @@
-import { CLEAR_EMAIL_VALIDITY } from '../actions/api';
+import { 
+    CHECK_EMAIL_VALIDITY, 
+    CLEAR_EMAIL_VALIDITY } from '../actions/api';
 
 const initialState = {};
 
@@ -6,7 +8,7 @@ export default function (state = initialState, action) {
     switch(action.type) {
         case CLEAR_EMAIL_VALIDITY:
             return {};
-        case CHECK:
+        case CHECK_EMAIL_VALIDITY:
             let nextState = {};
             if (action.payload.email != null)
                 nextState.email_validity = action.payload.email;
