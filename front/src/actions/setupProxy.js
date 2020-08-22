ad52ae6f6f5062da3f03e7d288e79eedd8f2f3f3
LEEJ0NGWAN <lastffx@gmail.com>
2020-08-23 02:30:18 +0900

    api host 설정 (나중에 환경변수에 따라 다이나믹해지도록 하기)

--- front/src/actions/setupProxy.js
+++ front/src/actions/setupProxy.js
@@ -1,2 +1,9 @@
-export const API_HOST = 'http://localhost:8000';
+// production
+// export const API_HOST = 'http://freechart/api';
+
+// local
+// export const API_HOST = 'http://freechart.local/api';
+
+// client side example
+export const API_HOST = 'http://192.168.0.9/api';
 
