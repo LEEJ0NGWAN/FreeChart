3bb19c1901d49cba9b1a22b950d9e6f7ec505466
LEEJ0NGWAN <lastffx@gmail.com>
2020-08-29 21:02:16 +0900

    local ip

--- front/src/actions/setupProxy.js
+++ front/src/actions/setupProxy.js
@@ -2,8 +2,9 @@
 // export const API_HOST = 'http://freechart/api';
 
 // local
-export const API_HOST = 'http://freechart.local/api';
+// export const API_HOST = 'http://freechart.local/api';
+// export const API_HOST = 'http://localhost/api';
 
 // client side example
 // export const API_HOST = 'http://192.168.0.9/api';
-
+export const API_HOST = 'http://192.168.0.12/api';
