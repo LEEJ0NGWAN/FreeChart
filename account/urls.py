0f581e6aac62fd4bfddc30a834acf2b4520f0505
LEEJ0NGWAN <lastffx@gmail.com>
2020-08-29 21:00:24 +0900

    테스트 api와 망령이된 로그아웃

--- account/urls.py
+++ account/urls.py
@@ -4,14 +4,12 @@ from rest_framework_simplejwt import views as jwt_views
 from . import views_rest
 
 urlpatterns = [
-    # path('login/', views_rest.login),
-    # path('logout/', views_rest.Logout.as_view()),
+    path('login/', views_rest.Login.as_view()),
     path('email/verify/', views_rest.EmailVerify.as_view()),
     path('password/reset/', views_rest.PasswordReset.as_view()),
     path('check/', views_rest.Check.as_view()),
     path('create/', views_rest.UserCreate.as_view()),
     path('delete/', views_rest.UserDelete.as_view()),
-    path('login/', jwt_views.TokenObtainPairView.as_view()),
     path('refresh/', jwt_views.TokenRefreshView.as_view()),
     path('verify/', views_rest.Auth.as_view()),
 ]
