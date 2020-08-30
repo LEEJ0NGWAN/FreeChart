a9e4136fca14335b5783cc91e37637b4943cc381
LEEJ0NGWAN <lastffx@gmail.com>
2020-08-31 02:05:31 +0900

    환경변수에서 api 호스트 가져오도록

--- front/src/actions/setupProxy.js
+++ front/src/actions/setupProxy.js
@@ -3,8 +3,6 @@
 
 // local
 // export const API_HOST = 'http://freechart.local/api';
-// export const API_HOST = 'http://localhost/api';
 
-// client side example
-// export const API_HOST = 'http://192.168.0.9/api';
-export const API_HOST = 'http://192.168.0.12/api';
+export const API_HOST = process.env.REACT_APP_API_HOST;
+
